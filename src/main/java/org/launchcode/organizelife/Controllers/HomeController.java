cb9829013bc3afc367a5e62094088d77f6bdb5b2
Ken Messmer <kenmessmerjr@gmail.com>
2019-08-04 15:15:57 -0500

    Added the ability to add new tasks but only individual tasks with no detail.

--- src/main/java/org/launchcode/organizelife/Controllers/HomeController.java
+++ src/main/java/org/launchcode/organizelife/Controllers/HomeController.java
@@ -3,16 +3,36 @@ package org.launchcode.organizelife.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 
 @Controller
-@RequestMapping("life")
+@RequestMapping("home")
 public class HomeController {
 
+    ArrayList<String> tasks = new ArrayList<>();
     @RequestMapping(value = "")
 
     public String index(Model model){
-        model.addAttribute("title", "Organize Your Life");
+
+
+        model.addAttribute("tasks", tasks);
+        model.addAttribute("title", "Organize Your Life!");
         return "home/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayTaskForm(Model model){
+        model.addAttribute("title", "New Tasks!");
+        return "home/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddTaskForm(@RequestParam String taskName){
+        tasks.add(taskName);
+        return "redirect:";
     }
 }
