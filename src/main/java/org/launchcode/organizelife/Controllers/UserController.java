a3893297fe13f77978eba4a96498bd84b4053f76
Ken Messmer <kenmessmerjr@gmail.com>
2019-08-22 22:56:13 -0500

    added some to the UserController still need to link it to the index and the add function.

--- src/main/java/org/launchcode/organizelife/Controllers/UserController.java
+++ src/main/java/org/launchcode/organizelife/Controllers/UserController.java
@@ -1,10 +1,20 @@
 package org.launchcode.organizelife.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
+    @RequestMapping(value = "user")
+    public String index(Model model){
+
+
+        model.addAttribute("user");
+        model.addAttribute("title", "Organize Your Life!");
+
+        return "home/index";
+    }
 }
