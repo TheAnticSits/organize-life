b5474087c8c8d0599528debdd4ea44ba8738d499
Ken Messmer <kenmessmerjr@gmail.com>
2019-08-25 22:43:17 -0500

    Working on Signup.  No Errors but still not fully functional.

--- src/main/java/org/launchcode/organizelife/Controllers/UserController.java
+++ src/main/java/org/launchcode/organizelife/Controllers/UserController.java
@@ -3,14 +3,23 @@ package org.launchcode.organizelife.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
-    @RequestMapping(value = "add")
+    @RequestMapping(value = "add", method = RequestMethod.GET)
     public String add(Model model){
+        model.addAttribute("title", "Create New User");
 
         return "user/add";
+    }
+    @RequestMapping( value = "add", method = RequestMethod.POST)
+    public String processAddUser(Model model, @RequestParam String taskName, @RequestParam String importance) {
+
+
+        return "redirect:";
     }
 }
