5900f1203f1914082cda1ddab9d1877c41bd0eb1
Ken Messmer <kenmessmerjr@gmail.com>
2019-08-27 16:22:04 -0500

    Added creating of new user to UserController.  Need to direct to a proper view.

--- src/main/java/org/launchcode/organizelife/Controllers/UserController.java
+++ src/main/java/org/launchcode/organizelife/Controllers/UserController.java
@@ -1,14 +1,19 @@
 package org.launchcode.organizelife.Controllers;
 
+import org.launchcode.organizelife.Models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+
 @Controller
 @RequestMapping("user")
 public class UserController {
+
+    static ArrayList<User> users = new ArrayList<>();
 
     @RequestMapping(value = "add", method = RequestMethod.GET)
     public String add(Model model){
@@ -17,8 +22,9 @@ public class UserController {
         return "user/add";
     }
     @RequestMapping( value = "add", method = RequestMethod.POST)
-    public String processAddUser(Model model, @RequestParam String taskName, @RequestParam String importance) {
-
+    public String processAddUser(Model model, @RequestParam String userName, @RequestParam String email, @RequestParam String password) {
+        User newUser = new User(userName, email, password);
+        users.add(newUser);
 
         return "redirect:";
     }
